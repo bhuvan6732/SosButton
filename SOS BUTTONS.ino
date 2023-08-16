f6ffb67a4f2f51129cba3229400bbec2669424d9
bhuvan6732 <85287192+bhuvan6732@users.noreply.github.com>
2023-08-16 23:10:31 +0530

    Script edited

--- SOS BUTTONS.ino
+++ SOS BUTTONS.ino
@@ -1,31 +1,58 @@
+#include <TinyGPS++.h>
+
+TinyGPSPlus gps;
+
 #include <SoftwareSerial.h>
+SoftwareSerial mySerial(3, 2);
+SoftwareSerial mySerial1(4, 5);
+char msg;
+char call;
 
-SoftwareSerial gsmSerial(3, 2); // RX, TX
-
+float Lat, Lon;
 void setup()
 {
-   gsmSerial.begin(9600); // Setting the baud rate of GSM Module
-   Serial.begin(9600);    // Setting the baud rate of Serial Monitor (Arduino)
-   delay(1000);
-   Serial.println("Preparing to send SMS");
-   SendMessage();
+  mySerial.begin(9600);
+  mySerial1.begin(38400);
+  Serial.begin(19200);
+  pinMode(12, INPUT);
+  pinMode(9, OUTPUT);
 }
-
 void loop()
 {
-}
-void SendMessage()
-{
-   Serial.println("Setting the GSM in text mode");
-   gsmSerial.println("AT+CMGF=1\r");
-   delay(2000);
-   Serial.println("Sending SMS to the desired phone number!");
-   gsmSerial.println("AT+CMGS=\"+919999999999\"\r");
-   // Replace x with mobile number
-   delay(2000);
+  int key = digitalRead(12);
 
-   gsmSerial.println("Hello from SIM900"); // SMS Text
-   delay(200);
-   gsmSerial.println((char)26); // ASCII code of CTRL+Z
-   delay(2000);
+  while (mySerial1.available() > 0)
+  {
+    gps.encode(mySerial1.read());
+    if (gps.location.isUpdated())
+    {
+      Lat = gps.location.lat();
+      Lon = gps.location.lng();
+    }
+  }
+
+  if (key == 0)
+  {
+    Serial.println("Setting the GSM in text mode");
+    digitalWrite(9, HIGH);
+    sendsms();
+    digitalWrite(9, LOW);
+  }
+}
+void sendsms()
+{
+
+  Serial.println("Setting the GSM in text mode");
+  mySerial.println("AT+CMGF=1\r");
+  delay(2000);
+  Serial.println("Sending SMS to the desired phone number!");
+  mySerial.println("AT+CMGS=\"+919999999999\"\r");
+  // Replace x with mobile number
+  delay(2000);
+  // mySerial.println("Help me please");
+  String msg = "I want Help !!!Location: Lat: " + String(Lat) + " Lon: " + String(Lon);
+  mySerial.println(msg);
+  delay(200);
+  mySerial.println((char)26); // ASCII code of CTRL+Z
+  delay(2000);
 }
